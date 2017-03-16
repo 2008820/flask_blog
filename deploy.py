6afc10db9b40651c037a1de2431eef0eeddb0789
di.xiao <di.xiao@socialcredits.cn>
2017-03-16 10:45:50 +0800

    增加部署脚本

---
+++ deploy.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# encoding=utf-8
+from fabric.api import *
+from fabric.contrib.files import exists
+from app.web_conf import host, host_passwd
+APP_PATH = "/root/blog"
+
+
+# @parallel(pool_size=12)
+def put_app():
+    if not exists(APP_PATH):
+        run("mkdir -p "+APP_PATH)
+    with settings(warn_only=True):
+        local("tar -zcvf blog_app.tar.gz * ")
+    put("blog_app.tar.gz", APP_PATH)
+    with cd(APP_PATH):
+        run("tar -zxvf blog_app.tar.gz")
+        run("rm blog_app.tar.gz")
+    local("rm blog_app.tar.gz")
+
+if __name__ == "__main__":
+    env.hosts = host
+    env.password = host_passwd
+    execute(put_app)
